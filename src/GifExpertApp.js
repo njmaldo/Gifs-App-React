5d56bf5ca84de8b6f4e73ca2d2ab7678347c691f
nestor Maldonado <nestorjmaldo@gmail.com>
2022-03-21 09:34:08 -0300

    Gifs-app

---
+++ src/GifExpertApp.js
@@ -0,0 +1,34 @@
+
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['Dragon']);
+    // const handleAdd = () =>{
+    //     //se puede añadir primero el personaje y depues ...categories
+    //     //setCategories([...categories, 'HonterX']);
+    //     //Tambien se puede así
+    //     setCategories(cat => [...categories, 'HonterX']);
+    // }
+
+  return (
+    <>
+        <h2>GifExpertApp</h2>
+        <AddCategory setCategories={ setCategories }/>
+        <hr/>
+
+        <ul>
+            {
+                categories.map(category => (
+                    <GifGrid 
+                        key={ category }
+                        category={ category }/>
+                ))
+            }
+        </ul>
+    </>
+  )
+}
